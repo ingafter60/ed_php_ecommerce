2993c5027745d35f7432e6087a10001bb59405d0
ing <ingafter60@gmail.com>
2020-09-17 09:11:41 +0700

    1. Basic setup

---
+++ resources/config.php
@@ -0,0 +1,30 @@
+<?php 
+   
+   ob_start();
+   
+   session_start();
+   
+   DEFINED("DS") ? null : DEFINE("DS", DIRECTORY_SEPARATOR);
+   
+   DEFINED("TEMPLATE_FRONT") ? null : DEFINE("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
+   
+   DEFINED("TEMPLATE_BACK") ? null : DEFINE("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
+   
+   DEFINED("UPLOAD_DIRECTORY") ? null : DEFINE("UPLOAD_DIRECTORY", __DIR__ . DS . "uploads");
+   
+   
+   DEFINED("DB_HOST") ? null : DEFINE("DB_HOST", "localhost");
+   
+   DEFINED("DB_USER") ? null : DEFINE("DB_USER","root");
+      
+   DEFINED("DB_PASS") ? null : DEFINE("DB_PASS", "");
+   
+   DEFINED("DB_NAME") ? null : DEFINE("DB_NAME",  "ed_php_ecommerce");
+   
+   
+   $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+   
+   require_once("functions.php");
+   
+   
+?>
---
+++ resources/functions.php
@@ -0,0 +1,6 @@
+<?php 
+
+// Testing the DB connection
+if($connection){
+   echo "You are connected with the DB!";
+}
