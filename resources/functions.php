99811c688242a52456b800946f4d2f2df0449c25
ing <ingafter60@gmail.com>
2020-09-17 10:33:34 +0700

    5. Create custom helper functions

--- resources/functions.php
+++ resources/functions.php
@@ -1,6 +1,31 @@
 <?php 
 
-// Testing the DB connection
-if($connection){
-   echo "You are connected with the DB!";
+// Redirect
+function redirect($location) {
+	header("Location: $location");
+}
+
+// Query
+function query($sql) {
+	global $connection;
+	return mysql_query($connection, $sql);
+}
+
+// Confirm function
+function confirm($result) {
+	global $connection;
+	if(!$result){
+			die("QUERY FAILED" . mysqli_error($connection));
+	}
+}
+
+// Escape string value
+function escape_string($string) {
+	global $connection;
+	return mysqli_real_escape_string($connection, $string);
+}
+
+// Fetch array
+function fetch_array($result) {
+	return mysqli_fetch_array($result);
 }
