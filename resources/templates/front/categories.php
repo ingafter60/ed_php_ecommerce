962708deacada9cf1c8dcb4fa48f3f8f85532328
ing <ingafter60@gmail.com>
2020-09-17 10:18:47 +0700

    4. Rendering categories items from the db to the homepage

--- resources/templates/front/categories.php
+++ resources/templates/front/categories.php
@@ -1,6 +1,25 @@
-            <p class="lead">Shop Name</p>
-            <div class="list-group">
-               <a href="category.html" class="list-group-item">Category 1</a>
-               <a href="#" class="list-group-item">Category 2</a>
-               <a href="#" class="list-group-item">Category 3</a>
-            </div>
+<p class="lead">Shop Name</p>
+<div class="list-group">
+
+	<?php  
+
+		$query 		= "SELECT * FROM categories";
+		$send_query = mysqli_query($connection, $query);
+
+		// Display error
+		if(!$send_query){
+			die("Query Failed" . mysqli_error($connection));
+		}
+
+		while ($row = mysqli_fetch_array($send_query)) {
+			
+			echo "
+				<a href='' 
+					class='list-group-item'>
+					{$row['cat_title']}
+				</a>";
+
+		}
+
+	?>
+</div>
