a97f042d7a321078f2df68771fcce040e5213bfd
ing <ingafter60@gmail.com>
2020-09-17 13:17:26 +0700

    9. Rendering page detail/item

--- resources/functions.php
+++ resources/functions.php
@@ -44,14 +44,21 @@ function get_products() {
 		$product = <<<DELIMETER
 			<div class="col-sm-4 col-lg-4 col-md-4">
 			   <div class="thumbnail">
-			      <img src="http://placehold.it/320x150" alt="">
+			   	<a href="item.php?id={$row['product_id']}">
+			      	<img src="{$row['product_image']}" alt="">
+			      </a>	
 			      <div class="caption">
-			         <h4 class="pull-right">$ {$row['product_price']}</h4>
-			         <h4><a href="#">Fifth Product</a>
+			         <h4 class="pull-right">&#36; {$row['product_price']}</h4>
+			         <h4><a href="#">{$row['product_title']}</a>
 			         </h4>
-			         <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>
-			         <a class="btn btn-primary" target="_blank" href="">Add to cart</a></p>
-			      </div>
+			         <p>See more snippets like this online store item at
+			         	<a target="_blank" 
+			         		href="">Bootsnipp</a>
+			         </p>
+			         <a class="btn btn-primary" 
+			         	target="_blank" 
+			         	href="item.php?id={$row['product_id']}">Add to cart</a>
+			      </div><br>
 			   </div>
 			</div>
 
